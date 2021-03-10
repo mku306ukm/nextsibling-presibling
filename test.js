40b784b55f1c480c478549b3df4b274c9aeab147
samod singh <samod0098@gmail.com>
2021-03-10 22:02:54 +0530

    nextsibling-presibling

---
+++ test.js
@@ -0,0 +1,8 @@
+
+document.getElementById("child-C").previousElementSibling.style.background = "red";
+
+
+var a = document.getElementById("child-C").previousElementSibling.innerHTML;
+
+console.log(a);
+
